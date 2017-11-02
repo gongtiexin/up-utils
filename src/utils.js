de0de886c4c29f6dd97c36b26c8b0c3984ab6ea0
tiexin.gong <tiexin.gong@qq.com>
2017-11-02 14:17:02 +0800

    添加getCookie方法

--- src/utils.js
+++ src/utils.js
@@ -119,15 +119,32 @@ const utils = {
 
   /**
    * 根据key去取obj里面的value
-   * 例如: this.delve({ a: { b: { c: 1 } } }, 'a.b.c') 输出　1
+   * 例如: this.delve({ a: { b: { c: 1 } } }, 'a.b.c') 输出 1
    * */
   delve(obj, key) {
     key.split('.').map((p) => {
       obj = obj && obj[p];
       return obj;
     }).pop();
-  }
-};
+  },
 
+  /**
+   * 根据cookie的key取出对应的value
+   * */
+  getCookie(cookieName) {
+    if (document.cookie.length > 0) {
+      let cookieStart = document.cookie.indexOf(`${cookieName}=`);
+      if (cookieStart !== -1) {
+        cookieStart = cookieStart + cookieName.length + 1;
+        let cookieEnd = document.cookie.indexOf(';', cookieStart);
+        if (cookieEnd === -1) {
+          cookieEnd = document.cookie.length;
+        }
+        return unescape(document.cookie.substring(cookieStart, cookieEnd));
+      }
+    }
+    return '';
+  },
+};
 
 export default utils;
